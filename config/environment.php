9ee8788b72a0067039b5479c381f6e9b5cc6f8c5
Gregory14 <gregory.joly.14@gmail.com>
2018-10-15 22:13:31 +0200

    Edit config environnement

--- config/environment.php
+++ config/environment.php
@@ -4,6 +4,6 @@
 // Define environment type
 /*----------------------------------------------------*/
 return [
-    'local' => ['MBP-de-Gregory','MacBook-Pro-de-Gregory.local'],
+    'local' => ['MBP-de-Gregory', 'MBPdeGregory2','MacBook-Pro-de-Gregory.local'],
     'production' => 'INSERT-HOSTNAME'
 ];
